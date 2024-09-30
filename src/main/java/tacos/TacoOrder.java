008f09e0e13893ed766f535e58a4bc7dfa8ca3e8
anosman <53019270+ANOsman@users.noreply.github.com>
2024-09-30 09:50:01 +0200

    Update TacoOrder.java

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -1,10 +1,14 @@
 package tacos;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.CreditCardNumber;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
@@ -21,15 +25,35 @@ public class TacoOrder implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    
+    @NotBlank(message="Delivery name is required")
     private String deliveryName;
+    
+    @NotBlank(message="City is required")
     private String deliveryCity;
+
+    @NotBlank(message="Street is required")
     private String deliveryStreet;
-    private String deliveryState;
-    private String deliveryZipCode;
+
+    @NotBlank(message="Region is required")
+    private String deliveryRegion;
+
+    @NotBlank(message="Zip code is required")
+    private String deliveryZip;
+
+    @CreditCardNumber(message="Not a valid credit card number")
     private String ccNumber;
+
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$",
+           message="Must be formatted MM/YY")
     private String ccExpiration;
+
+    @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccVV;
+    
     private Date placedAt = new Date();
+    
     @OneToMany(cascade = CascadeType.ALL)
     private List<Taco> tacos = new ArrayList<>();
 
