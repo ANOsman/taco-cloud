ad79c57ec6526a2fe4b49aa24e187a55f58296c3
anosman <53019270+ANOsman@users.noreply.github.com>
2024-09-30 09:44:07 +0200

    Update Taco.java

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.rest.core.annotation.RestResource;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import java.util.Date;
 import java.util.List;
@@ -18,8 +20,15 @@ public class Taco {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
+    
     private Date createdAt = new Date();
+    
     @ManyToMany
+    @NotNull
+    @Size(min=1, message="You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 }
